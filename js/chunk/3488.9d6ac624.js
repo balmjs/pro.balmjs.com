b118d77ed7a3672c42d01fd0f1eb47e9078b89ea
elf-mouse <ifmouz@gmail.com>
2022-12-19 14:10:32 +0800

    update official site for balm-ui-pro@0.22.0

---
+++ js/chunk/3488.9d6ac624.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbalm_ui_pro=self.webpackChunkbalm_ui_pro||[]).push([[3488],{3488:function(e,l,a){a.r(l),l.default=e=>[{label:"Label A",component:"ui-readonly-item",key:"a",value:e.value},{label:"Label B",component:"ui-readonly-item",key:"b",value:e=>e.value+"!"}]}}]);
